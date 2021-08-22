8c4b0dff857f20dab0ddf10b87082c9742f27a2a
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-22 01:59:13 -0700

    express

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+console.log("starting");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello world!" });
+});
+
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
